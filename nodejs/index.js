5a4d1d67103f9b400a86411641afccd80cd1df26
Paulo Diovani <paulo@diovani.com>
2016-10-02 17:42:35 -0300

    Split web and socket in separate modules

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,31 +1,17 @@
 'use strict';
 
-const Express = require('express');
 const LogSocket = require('debug')('socket');
 const LogWeb = require('debug')('web');
-const Net = require('net');
-const Path = require('path');
-const SocketClient = require('./lib/socket_client');
 
-const app = Express();
-
-// view engine setup
-app.set('views', Path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// routes
-app.get('/', (req, res) => {
-  res.render('index');
-});
+const SocketServer = require('./lib/socket_server');
+const WebServer = require('./lib/web_server');
 
 // start the Express App
-const httpServer = app.listen(process.env.WEB_PORT || 3000, function() {
-  LogWeb('Express server listening on port ' + httpServer.address().port);
+const server = WebServer.listen(process.env.WEB_PORT || 3000, function() {
+  LogWeb('Express server listening on port ' + server.address().port);
 });
 
-// start the Net server
-const socket = Net.createServer(SocketClient);
-
-socket.listen(process.env.SOCKET_PORT || 4000, () => {
-  LogSocket(`Socket server listening on port ${socket.address().port}`);
+// start the TCP Socket server
+SocketServer.listen(process.env.SOCKET_PORT || 4000, () => {
+  LogSocket(`Socket server listening on port ${SocketServer.address().port}`);
 });
